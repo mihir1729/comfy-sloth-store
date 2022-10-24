25f5ab2f9f10a54b1931979f2179a47823f1e8f4
Mihir Yadav <72518307+mihir1729@users.noreply.github.com>
2022-10-24 23:17:24 +0530

    checkout-setup

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
 	<Auth0Provider
-		domain='mihiryadav.us.auth0.com'
-		clientId='5NaqUJAJURabgPKbmid5dQ8q1egQ0x7U'
+		domain={process.env.REACT_APP_AUTH_DOMAIN}
+		clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
 		redirectUri={window.location.origin}
 		cacheLocation='localstorage'
 	>
